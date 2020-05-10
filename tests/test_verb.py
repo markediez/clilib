3d8a8062557edc716dc70aa42a25e3dd529c00ed
Mark Diez <markediez@gmail.com>
2020-05-10 15:56:22 -0700

    test out pytest and decorators

---
+++ tests/test_verb.py
@@ -0,0 +1,10 @@
+import clilib
+
+def test_verb_decorator(capsys):
+    @clilib.decorator.verb
+    def foo():
+        print("test verb")
+    
+    foo()
+    captured = capsys.readouterr()
+    assert captured.out == "verb wrap\ntest verb\nend verb wrap\n"

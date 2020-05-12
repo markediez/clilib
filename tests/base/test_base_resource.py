b4757b57ee04722cdacd4fe983bbefa6a49d2da8
Mark Diez <markediez@gmail.com>
2020-05-11 17:02:25 -0700

    Expand base class

--- tests/base/test_base_resource.py
+++ tests/base/test_base_resource.py
@@ -1,22 +1,57 @@
+import pytest
+import argparse
 import clilib.base
+from abc import ABC
+
+class Foo(clilib.base.BaseResource):
+        pass
+
 
 def test_base_resource_is_abc():
-    class Foo(clilib.base.BaseResource):
-        pass
-
-    assert(issubclass(Foo, clilib.base.BaseResource))
+    assert(issubclass(clilib.base.BaseResource, ABC))
 
 
-def test_subclass_inherits_parser():
-    class Foo(clilib.base.BaseResource):
-        pass
+def test_subclass_inherits_properties():
+    foo = Foo()
 
-    a = Foo("parser")
-    assert(a.parser == "parser")
+    assert(foo.parser == None)
+    assert(isinstance(foo.name, str) and foo.name == "")
+    assert(isinstance(foo.verbs, list) and len(foo.verbs) == 0)
 
-    a.parser = "modify"
-    assert(a.parser == "modify")
 
-    del a.parser
-    assert(hasattr(a, "parser") == False)
+def test_subclass_property_types_are_defined():
+    foo = Foo()
+    
+    foo.parser = argparse.ArgumentParser()
+    assert(isinstance(foo.parser, argparse.ArgumentParser))
 
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+    foo.parser = subparsers.add_parser('foo')
+    assert(isinstance(foo.parser, argparse.ArgumentParser))
+
+    with pytest.raises(AssertionError):
+        foo.parser = "assert fail"
+
+    foo.name = "foo"
+    assert(foo.name == "foo")    
+    with pytest.raises(AssertionError):
+        foo.name = 1
+    
+    foo.verbs = ["a"]
+    assert(len(foo.verbs) == 1 and foo.verbs[0] == "a")
+    with pytest.raises(AssertionError):
+        foo.verbs = "assert fail"
+
+
+def test_subclass_properties_are_deletable():
+    foo = Foo()
+
+    del foo.parser
+    assert(not hasattr(foo, "parser"))
+
+    del foo.name
+    assert(not hasattr(foo, "name"))
+
+    del foo.verbs
+    assert(not hasattr(foo, "verbs"))

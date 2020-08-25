e745ea44d5f7fae00020c097306ba5aca322be31
Mark Diez <markediez@gmail.com>
2020-08-25 01:35:11 -0700

    Add test for registering verb

--- src/clilib/decorator/verb.py
+++ src/clilib/decorator/verb.py
@@ -1,23 +1,10 @@
 import clilib
 
-SUBPARSERS = {}
+def verb(func):
+    def wrapper(*args, **kwargs):
+        resource = args[0]
+        clilib.register_verb(resource, func)
 
-def verb(verb):
-    def _verb(func):
-        def wrapper(*args, **kwargs):
-            return func(*args, **kwargs)
+        return func(*args, **kwargs)
 
-        if verb not in clilib._subparsers.choices:
-            clilib._subparsers.add_parser(verb)
-
-        if verb not in SUBPARSERS:
-            SUBPARSERS[verb] = clilib._subparsers.choices[verb].add_subparsers()
-
-        resource_name = clilib.util.to_kebab(clilib.util.get_resource_name(func))
-        if resource_name not in SUBPARSERS[verb].choices:
-            resource_parser = SUBPARSERS[verb].add_parser(resource_name)
-            resource_parser.set_defaults(func=func)
-
-        return wrapper
-
-    return _verb
+    return wrapper
--- tests/decorator/test_verb.py
+++ tests/decorator/test_verb.py
@@ -1,10 +1,19 @@
-import clilib
+from clilib.decorator import resource, verb
 
-# def test_verb_decorator(capsys):
-#     @clilib.decorator.verb("get")
-#     def foo():
-#         print("test verb")
-    
-#     foo()
-#     captured = capsys.readouterr()
-#     assert captured.out == "verb wrap\ntest verb\nend verb wrap\n"
+class TestDecoratorVerb():
+    def test_decorated_function_registers_as_a_verb(self):
+        @resource
+        class MyResource():
+            @verb
+            def get(self):
+                print("Got something")
+
+        # Need to run the function for decorators to compile
+        # TODO: Figure out a way to not do this... check Flask code?
+        MyResource().get()
+        assert 'get' in getattr(MyResource, '__parsers')
+
+
+    def test_specify_verb_through_decorator(self):
+        pass
+

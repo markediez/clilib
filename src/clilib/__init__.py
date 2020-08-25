8d4e2e4686d1b4072f12fc3ff8bc8b8d142a1280
Mark Diez <markediez@gmail.com>
2020-08-25 01:34:42 -0700

    Add function to register a verb and resource

--- src/clilib/__init__.py
+++ src/clilib/__init__.py
@@ -13,6 +13,21 @@ def build_parser(parser):
     return parser
 
 
+def register_verb(resource, func):
+    resource_name = clilib.util.to_kebab(resource.__class__.__name__)
+    verb = func.__name__
+
+    if verb not in _subparsers.choices:
+        _subparsers.add_parser(verb)
+
+    if verb not in resource.__class__.__parsers:
+        resource.__class__.__parsers[verb] = _subparsers.choices[verb].add_subparsers()
+
+    if resource_name not in resource.__class__.__parsers[verb].choices:
+        resource_parser = resource.__class__.__parsers[verb].add_parser(resource_name)
+        resource_parser.set_defaults(func = func)
+
+
 def run(prog):
     global _root_parser
     global _args
@@ -20,4 +35,7 @@ def run(prog):
     _root_parser.prog = prog
     _args = _root_parser.parse_args()
 
-    _args.func(_args)
+    print(_args)
+
+    if hasattr(_args, "func"):
+        _args.func(_args)

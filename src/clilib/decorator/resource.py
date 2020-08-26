50c3eaf3d3549a6f6e326309501d34e2e9660f68
Mark Diez <markediez@gmail.com>
2020-08-26 10:32:21 -0700

    Detect and register verb functions

--- src/clilib/decorator/resource.py
+++ src/clilib/decorator/resource.py
@@ -1,12 +1,13 @@
+import clilib
+from . import verb
+
 def resource(klass):
-    print("DECORATING RSRC")
     setattr(klass, '__parsers', {})
     setattr(klass, '__name', '')
 
     for key, value in klass.__dict__.items():
-        print(f"Checking '{key}'")
-        if callable(value):
-            print('Function found. Adding attr')
-            setattr(value, '__resource', klass)
+        if callable(value) and verb.is_verb(value):
+            clilib.register_verb(klass, value)
+
 
     return klass
--- tests/decorator/test_resource.py
+++ tests/decorator/test_resource.py
@@ -1,4 +1,4 @@
-from clilib.decorator import resource
+from clilib.decorator.resource import resource
 
 class TestDecoratorResource():
     def test_decorated_class_gets_base_attrs(self):
@@ -9,13 +9,3 @@ class TestDecoratorResource():
         assert MyResource.someattr == 'attr'
         assert hasattr(MyResource, '__parsers')
         assert hasattr(MyResource, '__name')
-
-
-    def test_decorated_class_methods_gets_resource_attribute(self):
-        @resource
-        class MyResource():
-            def get(self):
-                print("some func")
-
-        assert hasattr(MyResource.get, '__resource')
-        assert getattr(MyResource.get, '__resource') is MyResource

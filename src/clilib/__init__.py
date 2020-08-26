9cce607d3a0c4106faf36e60c34547b8d16ee952
Mark Diez <markediez@gmail.com>
2020-08-26 10:35:48 -0700

    Register from resource

--- src/clilib/__init__.py
+++ src/clilib/__init__.py
@@ -14,28 +14,27 @@ def build_parser(parser):
 
 
 def register_verb(resource, func):
-    resource_name = clilib.util.to_kebab(resource.__class__.__name__)
-    verb = func.__name__
+    resource_name = clilib.util.to_kebab(resource.__name__)
+    verb = getattr(func, '__action')
 
     if verb not in _subparsers.choices:
         _subparsers.add_parser(verb)
 
-    if verb not in resource.__class__.__parsers:
-        resource.__class__.__parsers[verb] = _subparsers.choices[verb].add_subparsers()
+    if verb not in resource.__parsers:
+        resource.__parsers[verb] = _subparsers.choices[verb].add_subparsers()
 
-    if resource_name not in resource.__class__.__parsers[verb].choices:
-        resource_parser = resource.__class__.__parsers[verb].add_parser(resource_name)
+    if resource_name not in resource.__parsers[verb].choices:
+        resource_parser = resource.__parsers[verb].add_parser(resource_name)
         resource_parser.set_defaults(func = func)
 
 
 def run(prog):
     global _root_parser
     global _args
+    global _subparsers
 
     _root_parser.prog = prog
     _args = _root_parser.parse_args()
-
-    print(_args)
 
     if hasattr(_args, "func"):
         _args.func(_args)

08cf0e9d7b7ad080b82bf3eb67c426cec1178261
Mark Diez <markediez@gmail.com>
2020-08-25 23:51:24 -0700

    Place class identifier for each resource function

--- src/clilib/decorator/resource.py
+++ src/clilib/decorator/resource.py
@@ -3,6 +3,10 @@ def resource(klass):
     setattr(klass, '__parsers', {})
     setattr(klass, '__name', '')
 
-        return klass
+    for key, value in klass.__dict__.items():
+        print(f"Checking '{key}'")
+        if callable(value):
+            print('Function found. Adding attr')
+            setattr(value, '__resource', klass)
 
     return klass
--- tests/decorator/test_resource.py
+++ tests/decorator/test_resource.py
@@ -9,3 +9,13 @@ class TestDecoratorResource():
         assert MyResource.someattr == 'attr'
         assert hasattr(MyResource, '__parsers')
         assert hasattr(MyResource, '__name')
+
+
+    def test_decorated_class_methods_gets_resource_attribute(self):
+        @resource
+        class MyResource():
+            def get(self):
+                print("some func")
+
+        assert hasattr(MyResource.get, '__resource')
+        assert getattr(MyResource.get, '__resource') is MyResource

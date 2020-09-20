be87e4c15efb3d451129b2d639b2838d283d0420
Mark Diez <markediez@gmail.com>
2020-09-20 12:02:20 -0700

    Add test

--- tests/decorator/test_arg.py
+++ tests/decorator/test_arg.py
@@ -3,28 +3,18 @@ import clilib.decorator.verb as verb
 import clilib.decorator.arg as arg
 
 
+@resource
+@arg('--environment', '-e', type=str, default='dev')
+class ArgTestResource():
+    @verb
+    @arg('positional_foo', type=str, default='')
+    def get(self, args):
+        print("Got something")
+
+
 class TestDecoratorArg():
     def test_decorated_function_adds_arg(self):
-        @arg('--environment', '-e', type=str, default='dev')
-        @resource
-        class ArgTestResource():
-            @arg('positional_foo', type=str, default='')
-            @verb
-            def get(self, args):
-                print("Got something")
-                print(f"positional foo: {args._positional_foo}")
-
         assert hasattr(ArgTestResource.get, '_args')
+
+    def test_decorated_resource_adds_arg(self):
         assert hasattr(ArgTestResource, '_args')
-
-    def test_positional_arg_exists(self):
-        pass
-
-    def test_flagged_arg_exists(self):
-        pass
-
-    def test_multiple_args_exists(self):
-        pass
-
-    def test_decorator_as_class_adds_arg_to_all(self):
-        pass
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -22,6 +22,20 @@ class FooResource():
         print("list FooResource")
 
 
+@clilib.decorator.resource
+@clilib.decorator.arg('--dry-run', action='store_true')
+@clilib.decorator.arg('--environment', '-e', type=str, choices=['dev', 'uat', 'stage', 'prod'])
+class ArgResource():
+    @clilib.decorator.verb
+    @clilib.decorator.arg('pos_arg', type=str)
+    def get(self, args):
+        print(f"get ArgResource {args.pos_arg}")
+
+    @clilib.decorator.verb
+    def list(self, args):
+        print(f"list ArgResource dry_run {args.dry_run} and env {args.environment}")
+
+
 class TestCli:
     def test_parser_prog_name_is_value_passed(self):
         clilib.init("clilib")
@@ -60,3 +74,48 @@ class TestCli:
             "other-resource"
         ])
         assert(args._func == OtherResource.get)
+
+    def test_positional_arg_exists(self):
+        args = clilib._root_parser.parse_args([
+            "get",
+            "arg-resource",
+            "pug"
+        ])
+
+        assert hasattr(args, 'pos_arg')
+        assert args.pos_arg == 'pug'
+
+    def test_decorator_as_class_adds_arg_to_all(self):
+        args = clilib._root_parser.parse_args([
+            "get",
+            "arg-resource",
+            "pug",
+            "--environment",
+            "dev"
+        ])
+
+        args2 = clilib._root_parser.parse_args([
+            "list",
+            "arg-resource",
+            "--environment",
+            "uat"
+        ])
+
+        assert hasattr(args, 'environment')
+        assert hasattr(args2, 'environment')
+        assert args.environment == 'dev'
+        assert args2.environment == 'uat'
+
+    def test_multiple_args(self):
+        args = clilib._root_parser.parse_args([
+            "list",
+            "arg-resource",
+            "--environment",
+            "uat",
+            "--dry-run"
+        ])
+
+        assert hasattr(args, "environment")
+        assert hasattr(args, "dry_run")
+        assert args.environment == 'uat'
+        assert args.dry_run
